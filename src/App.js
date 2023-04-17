9f41574edd9446b4e6741879013d7007e1ac42b1
Saurabh Pandey <rajkumarsaurabh123@gmail.com>
2023-04-18 01:15:29 +0530

    Removed warnings

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ import Header from "./components/Header";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import Slider from "react-slick";
-import { useState } from "react";
 import Footer from "./components/Footer";
 
 function App() {
